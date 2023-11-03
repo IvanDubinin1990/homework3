07871fe7d6801fd9761c6cbd24abe259490a1da2
Кристина <dubininivan1990@gmail.com>
2023-11-03 16:57:10 +0400

    Создан конвертер валют из рублей в евро и доллары с вводом значения от пользователя, проверкой на нежелательные значения.

---
+++ Homework3/src/Lesson3_2/Main.java
@@ -0,0 +1,45 @@
+package Lesson3_2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        raschetValut(proverka());
+
+    }
+
+    static void raschetValut(float x) {
+        float dollar = x / 92.94f;
+        float euro = x / 98.87f;
+        String formatterFloatDollar = String.format("%.2f", dollar);
+        String formatterFloatEuro = String.format("%.2f", euro);
+        System.out.println(x + " рублей это: " + formatterFloatDollar + " долларов или " + formatterFloatEuro + " евро.");
+
+    }
+
+    static float proverka() {
+
+        while (true) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите количество рублей: ");
+
+            try {
+
+                float x = sc.nextFloat();
+
+                if (x > 0) {
+                    return x;
+                } else {
+                    System.out.println("Вы ввели отрицательное число или 0");
+                }
+
+            }catch (InputMismatchException z){
+                System.out.println("Вы ввели не число, попробуйте ещё раз");
+            }
+        }
+    }
+}
